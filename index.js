25509f1a5b5c89476f1b98932439e55c20bb1dd9
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2024-11-23 19:36:24 +0600

    Delete, update find

--- index.js
+++ index.js
@@ -18,15 +18,28 @@ app.post("/insertdata", async (req, res) => {
     });
 
     await himodelInstance.save();
-    res.status(201).send({ message: "Data inserted" });
+    res.status(201).send({ message: "Data inserted", data: himodelInstance });
   } catch (error) {
     console.error(error);
     res.status(500).send({ message: "Internal server error" });
   }
 });
 
-app.get("/", (req, res) => {
-  res.status(200).send("How r you");
+app.get("/getalldata", async (req, res) => {
+  let getalldata = await himodel.find({});
+  res.status(200).send({ message: "Show all data", data: getalldata });
+});
+app.delete("/deletedata/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedata = await himodel.findOneAndDelete({ _id: id });
+  res.status(200).send({ message: "Deleted data", data: deletedata });
+});
+
+app.patch("/updatedata/:id", async (req, res) => {
+  let { id } = req.params;
+  let { name } = req.body;
+  let updatedata = await himodel.findOneAndUpdate({ _id: id }, { name: name });
+  res.status(200).send({ message: "update data", data: updatedata });
 });
 
 app.listen(port, (req, res) => {
